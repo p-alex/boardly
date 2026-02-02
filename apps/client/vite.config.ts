f6fae28db6a987d53d16f8d1bcda3a2ba19ebaef
p-alex <alex.daniel9021@gmail.com>
2026-02-02 16:54:21 +0200

    chore: set up client app

---
+++ apps/client/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    port: 5173,
+    host: true,
+  },
+  server: {
+    port: 5173,
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
